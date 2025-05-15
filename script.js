4754df0a5cadcb9d31460f355752087108a6dcaf
Olatunji Adekola <olatunjiadekola5@gmail.com>
2025-05-15 16:43:47 +0100

    Add button under box with color toggle

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello, World!");
+
+const button = document.getElementById('myButton');
+const box = document.getElementById('myBox');
+
+button.addEventListener('click', () => {
+  // Toggle between two background colors
+  if (box.style.backgroundColor === 'lightblue') {
+    box.style.backgroundColor = 'lightgreen';
+  } else {
+    box.style.backgroundColor = 'lightblue';
+  }
+});
