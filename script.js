8d085ad2ab53f9f9dc6ccb1be73180a1ac234499
Babajide Ibiayo <babajideibiayo@yahoo.com>
2025-05-17 07:16:53 +0100

    modified JS to allow toggle box backgound color

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-
 // 1. Add the click event listener to the button that will change the background color of the box to red
 
 // Add your JavaScript code here
 
 // Wait for the DOM to load
 
+const button = document.querySelector("button")
+const box = document.querySelector(".box")
 
-  const button = document.querySelector("button");
-  const box = document.querySelector(".box");
+if (button && box) {
+  button.addEventListener("click", function () {
+    //Changing the buttons text FROM "Click Me" to "Toggle"
+    button.textContent = "Toggle"
 
-  if (button && box) {
-    button.addEventListener("click", function () {
-      // Set background color
-      box.style.backgroundColor = "red" 
-      
-      //Changing the buttons text FROM "Click Me" to "Toggle"
-      button.textContent = "Toggle"
-      
-       // Toggle Box background color
-       box.style.backgroundColor = box.style.backgroundColor === "red" ? "" : "red"
-    });
-  }
+    // Toggle Box background color
+    box.style.backgroundColor = box.style.backgroundColor === "red" ? "" : "red"
+  })
+}
